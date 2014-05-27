1312df586dcb12bb226add43034dc40eb911f93e
timo.schmidt <timo-schmidt@gmx.net>
2014-05-27 16:08:58 +0200

    Added new Schema Classes

---
+++ src/SolrRestApiClient/Api/Client/Domain/Schema/Field/DynamicField.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace SolrRestApiClient\Api\Client\Domain\Schema\Field;
+
+/**
+ * Class DynamicField
+ *
+ * @author Timo Schmidt <timo.schmidt@gmx.net>
+ * @package SolrRestApiClient\Api\Client\Domain\Schema
+ */
+class DynamicField extends AbstractField {
+
+	/**
+	 * @param $concreteFieldName
+	 * @return boolean
+	 */
+	public function getIsNameMatching($concreteFieldName) {
+
+	}
+}
---
+++ src/SolrRestApiClient/Api/Client/Domain/Schema/Field/FieldCollection.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SolrRestApiClient\Api\Client\Domain\Schema\Field;
+
+use SolrRestApiClient\Api\Client\Domain\AbstractCollection;
+
+/**
+ * Class FieldCollection
+ *
+ * @author Timo Schmidt <timo.schmidt@aoe.com>
+ * @package SolrRestApiClient\Api\Client\Domain\StopWord
+ */
+class FieldCollection extends AbstractCollection {}
